596e00e94122533300c9bfcb4c75ee3cfd29055e
Neave Paul <37831381+neavepaul@users.noreply.github.com>
2023-09-12 18:10:28 +0530

    pipeline for testing

--- testing/pipeline.py
+++ testing/pipeline.py
@@ -1,29 +1,17 @@
-import kafka
-import json
-import os
 import concurrent.futures
-import urllib.request
 import pdfplumber
+import random
+import os
 import pandas as pd
+from retrying import retry
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from db_models import Road, Surveyor, Test, Subtest, QCR1Report, QCR2Report, QMRReport
 
-# Define your Kafka configuration
-kafka_config = {
-    "bootstrap_servers": "kafka_broker_url",
-    "group_id": "pdf_upload_consumer_group",
-    "auto_offset_reset": "earliest"
-}
-
-# Create a Kafka consumer for the PDF upload topic
-consumer = kafka.KafkaConsumer("pdf_upload_topic", **kafka_config)
-
-local_path = ""
-
 # Define your database connection URL
-db_url = "postgresql://username:password@localhost/SIH"
+db_url = "postgresql://username:password@localhost:5432/SIH"
 
+# Define a function to extract metadata
 def extract_metadata(page_text):
     metadata = {
         "Date": "",
@@ -39,6 +27,7 @@ def extract_metadata(page_text):
     return metadata
 
 # Define a function to process a single PDF
+@retry(wait_fixed=1000, stop_max_attempt_number=3)
 def process_pdf(pdf_path, report_type):
     with pdfplumber.open(pdf_path) as pdf:
         # Initialize dataframes for metadata and test results
@@ -64,31 +53,37 @@ def process_pdf(pdf_path, report_type):
                     tb = table.extract()
                     df = pd.DataFrame(tb[1:], columns=tb[0])
                     test_results_df = pd.concat([test_results_df, df], ignore_index=True)
-            
+
         # Add metadata columns to test_results
         test_results_df["Date"] = metadata_df["Date"][0]
         test_results_df["Surveyor Name"] = metadata_df["Surveyor's Name"][0]
         test_results_df["Road Name"] = metadata_df["Road Name"][0]
+        # Add "Report Type" column and fill with "qcr1"
         test_results_df["Report Type"] = str(report_type)
-        os.remove(local_path)
+
         return test_results_df
 
-# Define a function for transformation and loading
-def transform_and_load_data(pdf_path, db_url, report_type):
+# Define a function to transform and load data
+def transform_and_load_data(pdf_path, db_url):
     try:
-        test_results = process_pdf(pdf_path, report_type)
+        # PDF processing and data transformation code here
+        test_results = process_pdf(pdf_path)
 
+        # Create a connection to the PostgreSQL database
         engine = create_engine(db_url)
         Session = sessionmaker(bind=engine)
         session = Session()
 
+        # Iterate through the DataFrame and perform transformations
         for index, row in test_results.iterrows():
-            road = session.query(Road).filter_by(name=row['Road Name']).first()
-            surveyor = session.query(Surveyor).filter_by(name=row['Surveyor Name']).first()
-            test = session.query(Test).filter_by(name=row['Test Name']).first()
-            subtest = session.query(Subtest).filter_by(name=row['Subtest Name']).first()
+            # Lookup or insert Road, Surveyor, Test, Subtest
+            road = session.query(Road).filter_by(name=row['road_name']).first()
+            surveyor = session.query(Surveyor).filter_by(name=row['surveyor_name']).first()
+            test = session.query(Test).filter_by(name=row['test_name']).first()
+            subtest = session.query(Subtest).filter_by(name=row['subtest_name']).first()
 
-            report_type = row['Report Type']
+            # we'll get this from the kafka message
+            report_type = random.choice(['qcr1', 'qcr2', 'qmr'])
 
             # Create a new row in the appropriate table with the transformed values
             if report_type == 'qcr1':
@@ -132,36 +127,29 @@ def transform_and_load_data(pdf_path, db_url, report_type):
 
             session.add(report)
 
+        # Commit the changes to the database
         session.commit()
+
+        # Close the session and database connection
         session.close()
         engine.dispose()
 
     except Exception as e:
+        # Handle exceptions, log errors, or retry if needed
         print(f"Error processing PDF: {str(e)}")
         raise
 
-def download_pdf_from_firestore(url, name):
-    response = urllib.request.urlopen(url)
-    file = open(name, 'wb')
-    file.write(response.read())
-    file.close()
-    path = os.path.join(os.getcwd(), name)
-    return path
-
+# List of PDF paths to process
+pipeline_folder = 'buffer'
+pdf_paths = [os.path.join(pipeline_folder, filename) for filename in os.listdir(pipeline_folder) if filename.endswith('.pdf')]
 
 # Number of parallel workers (adjust as needed)
 num_workers = 4
 
 # Create a ThreadPoolExecutor with the desired number of workers
 with concurrent.futures.ThreadPoolExecutor(max_workers=num_workers) as executor:
-    for message in consumer:
-        message_data = json.loads(message.value)
-        pdf_url = message_data.get("pdf_url")
-        report_type = message_data.get("report_type")
-        file_name = message_data.get("file_name")
+    # Submit PDF processing tasks
+    futures = [executor.submit(transform_and_load_data, pdf_path, db_url) for pdf_path in pdf_paths]
 
-        # Download the PDF using the Firestore URL
-        local_path = download_pdf_from_firestore(pdf_url, file_name)
-
-        # Submit PDF processing tasks to the ThreadPoolExecutor
-        executor.submit(transform_and_load_data, local_path, report_type)
+    # Wait for all tasks to complete
+    concurrent.futures.wait(futures)
